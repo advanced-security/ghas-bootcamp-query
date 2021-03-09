7d65471a13a7e2e895598404f173149f85b7698d
Remco Vermeulen <rvermeulen@users.noreply.github.com>
2021-03-09 13:56:42 +0100

    Initialized project

---
+++ go/test/jwt-go/UnvalidatedSigningMethod/test.go
@@ -0,0 +1,32 @@
+package ghas_bootcamp
+
+import "github.com/dgrijalva/jwt-go"
+
+func unvalidated_case() {
+	token, err := jwt.ParseWithClaims(tokenString, &OctoClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(configuration.Secret), nil
+	})
+
+	if err != nil {
+		log.Printf("AuthN: Invalid token %s", err)
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+}
+
+func validated_case() {
+	token, err := jwt.ParseWithClaims(tokenString, &OctoClaims{}, func(token *jwt.Token) (interface{}, error) {
+
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(configuration.Secret), nil
+	})
+
+	if err != nil {
+		log.Printf("AuthN: Invalid token %s", err)
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+}
